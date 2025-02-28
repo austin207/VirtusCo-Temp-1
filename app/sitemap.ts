346051a20cd3904a8cf9ac8b3d54bcb4a345488b
austin207 <austinantony06@gmail.com>
2025-02-28 17:43:47 +0530

    Readme,license

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,7 +9,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: 'https://virtusco.in/about',
+      url: 'https://virtusco.in/robots',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
